9c735fb3a3814e0b6fba7e949cef131729b01bbb
“ <>
2025-09-04 10:40:57 -0600

    quinto commit

--- portafolio-senacit/src/App.jsx
+++ portafolio-senacit/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edita <code>src/App.jsx</code> guarda para probar HMR
         </p>
       </div>
       <p className="read-the-docs">
