f136b5e0d7aedad6ac546ba24279b6c81620e80f
“ <>
2025-09-04 10:39:31 -0600

    cuarto commit

--- portafolio-senacit/src/App.jsx
+++ portafolio-senacit/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Clic en el logo de Vite y React para mas información
       </p>
     </>
   )
